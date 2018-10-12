8553ade37a2030d95d708c3fad6af4019b40ee89
Talisa Erazo <talisaerazo@gmail.com>
2018-10-12 11:00:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,5 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if "#{number}" % 4 == 0
+    return "Fizz"
+  end
+end
