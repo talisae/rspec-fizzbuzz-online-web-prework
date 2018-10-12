374fe01cba7fb52c7b3a06a8d5b4e641655603f4
Talisa Erazo <talisaerazo@gmail.com>
2018-10-12 11:31:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -11,15 +11,3 @@ def fizzbuzz(number)
     return "nil"
   end
 end
-
-def fizzbuzz(int)
-if int % 3 == 0 && int % 5 == 0
-"FizzBuzz"
-elsif int % 3 == 0
-"Fizz"
-elsif int % 5 == 0
-"Buzz"
-else int % 4 == 0
-nil
-end
-end
