2d039fcc50ac4ad7a81540e06d7a6b5e4dc25233
Talisa Erazo <talisaerazo@gmail.com>
2018-10-12 11:32:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,20 @@ def fizzbuzz(number)
     return "Fizz"
   elsif number % 5 == 0
     return "Buzz"
-  else number % 4 == 0
-    return "nil"
+  else number % 4 == 0 ||
+    number % 5 != 0
+    return nil
   end
+end
+
+def fizzbuzz(int)
+if int % 3 == 0 && int % 5 == 0
+"FizzBuzz"
+elsif int % 3 == 0
+"Fizz"
+elsif int % 5 == 0
+"Buzz"
+else int % 4 == 0
+nil
+end
 end
